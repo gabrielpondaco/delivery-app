cfab8321d4d551c3bc439b3d33d6491c1e7817da
lucasbaq <lucasbaquintela@gmail.com>
2022-09-21 16:25:35 -0300

    inicia o controller de produtos

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { showAll } = require('../controllers/productController');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.route('/products').get(showAll);
 
 module.exports = app;
