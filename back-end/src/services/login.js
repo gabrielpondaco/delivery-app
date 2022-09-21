46c8e5b702273e94888ba5f72c5eaa497a43cead
rcolombodev <rcolombodev@gmail.com>
2022-09-21 17:02:33 -0300

    feat: cria service de login

---
+++ back-end/src/services/login.js
@@ -0,0 +1,30 @@
+const md5 = require('md5');
+const { Users } = require('../database/models');
+
+module.exports = {
+  async login(data) {
+    const user = await Users.findOne({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (!user) {
+      return { code: 404, message: 'Not Found' };
+    }
+
+    const IsValidPassword = md5(data.password) === user.password;
+
+    if (!IsValidPassword) {
+      return { code: 401, message: 'Unauthorized - Invalid Password' };
+    }
+
+    const userInfos = {
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+
+    return userInfos;
+  },
+};
