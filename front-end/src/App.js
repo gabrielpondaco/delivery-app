09e25f57999f120fc1752ffc1c573ad68f296ecc
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-09-21 14:41:53 -0300

    feat: adiciona pagina register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import Register from './pages/Register';
+import Products from './pages/Products';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/" element={ <Login /> } />
+      <Route exact path="/register" element={ <Register /> } />
+      <Route exact path="/customer/products" element={ <Products /> } />
       <Route path="/*" element={ <NotFound /> } />
     </Routes>
   );
