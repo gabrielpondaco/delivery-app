03ec389a3a7f276fdeaa914a7d8c8d0bc953e9c6
rcolombodev <rcolombodev@gmail.com>
2022-09-22 06:32:04 -0300

    feat: Adiciona register ao controller de login

--- back-end/src/controllers/login.js
+++ back-end/src/controllers/login.js
@@ -3,9 +3,18 @@ const LoginService = require('../services/login');
 
 module.exports = {
   async login(req, res) {
-    const UserData = await LoginService.login(req.body);
-    if (UserData.status) return res.status(UserData.status).json({ error: UserData.message });
-    const tokenGenerated = jwt.generate(UserData.email, UserData.role);
-    return res.status(200).json({ ...UserData, token: tokenGenerated });
+    const data = req.body;
+    const user = await LoginService.login(data);
+    if (user.status) return res.status(user.status).json({ error: user.message });
+    const tokenGenerated = jwt.generate(user.email, user.role);
+    return res.status(200).json({ ...user, token: tokenGenerated });
+  },
+
+  async register(req, res) {
+    const data = req.body;
+    const user = await LoginService.register(data);
+    if (user.status) return res.status(user.status).json({ error: user.message });
+    const tokenGenerated = jwt.generate(user.email, user.role);
+    return res.status(200).json({ ...user, token: tokenGenerated });
   },
 };
