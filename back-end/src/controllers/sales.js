5e2ae8867cdbd5783cedc9e37d9f189251bf48e6
thiagoguarino <thiago.guarino@outlook.com>
2022-09-22 15:22:27 -0300

    feat: sales controller functions

---
+++ back-end/src/controllers/sales.js
@@ -0,0 +1,39 @@
+const service = require('../services/index');
+
+const createOrder = async (req, res) => {
+  const newOrder = req.body;
+
+  await service.createOrder(newOrder);
+
+  return res.status(200).end();
+}
+
+const getAllClientsOrders = async (_req, res) => {
+  const orders = await service.getAllClientsOrders();
+
+  return res.status(200).json(orders);
+}
+
+const getByClientOrder = async (req, res) => {
+  const { id } = req.params;
+
+  const order = await service.getByClientOrder(id);
+
+  return res.status(200).json(order);
+}
+
+const updateOrderStatus = async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  await service.updateOrderStatus(id, status);
+
+  return res.status(200).end();
+}
+
+module.exports = {
+  createOrder,
+  getAllClientsOrders,
+  getByClientOrder,
+  updateOrderStatus,
+};
