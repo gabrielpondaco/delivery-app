fe18e8acafb97f5fd231b0358cc4b3b27dfdd077
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-09-22 15:55:02 -0300

    fix: corrige erros de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,9 +4,9 @@ const { loginRouter } = require('../routes');
 const app = express();
 
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
