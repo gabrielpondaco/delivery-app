f9cda6e531613fc6f5dc8992da4a611f26f7a0cb
Marcos Campos <marxmyers@icloud.com>
2022-09-22 18:07:12 -0300

    feat: little changes

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -7,28 +7,24 @@ function Header() {
     <nav>
       <Link
         to="/customer/products"
-        className="p1"
         data-testid="customer_products__element-navbar-link-products"
       >
         Produtos
       </Link>
       <Link
         to="/customer/orders"
-        className="p2"
         data-testid="customer_products__element-navbar-link-orders"
       >
         Meus Pedidos
       </Link>
       <Link
         to
-        className="p3"
         data-testid="customer_products__element-navbar-user-full-name"
       >
         Nome
       </Link>
       <Link
         to="/login"
-        className="p4"
         data-testid="customer_products__element-navbar-link-logout"
       >
         Sair
