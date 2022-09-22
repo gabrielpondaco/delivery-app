5282d4dd1948818b4e35cd6894a895331ba558e9
Marcos Campos <marxmyers@icloud.com>
2022-09-22 18:05:21 -0300

    feat: customer nav bar component

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/Header.css';
 
 function Header() {
   return (
     <nav>
-      <p customer_products__element-navbar-link-products>Produtos</p>
-      <p customer_products__element-navbar-link-orders>Meus Pedidos</p>
-      <p customer_products__element-navbar-user-full-name>Nome</p>
-      <p customer_products__element-navbar-link-logout>Sair</p>
+      <Link
+        to="/customer/products"
+        className="p1"
+        data-testid="customer_products__element-navbar-link-products"
+      >
+        Produtos
+      </Link>
+      <Link
+        to="/customer/orders"
+        className="p2"
+        data-testid="customer_products__element-navbar-link-orders"
+      >
+        Meus Pedidos
+      </Link>
+      <Link
+        to
+        className="p3"
+        data-testid="customer_products__element-navbar-user-full-name"
+      >
+        Nome
+      </Link>
+      <Link
+        to="/login"
+        className="p4"
+        data-testid="customer_products__element-navbar-link-logout"
+      >
+        Sair
+      </Link>
     </nav>
   );
 }
---
+++ front-end/src/pages/Orders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Orders() {
+  return (
+    <main>
+      <Header />
+      <h1>Orders</h1>
+    </main>
+  );
+}
+
+export default Orders;
