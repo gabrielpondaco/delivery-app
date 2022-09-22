8ea7e0c232e7c1764cb996cede8a7bd3e4d31cb8
Marcos Campos <marxmyers@icloud.com>
2022-09-22 16:35:18 -0300

    feat: creating Nav bar

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav>
+      <p customer_products__element-navbar-link-products>Produtos</p>
+      <p customer_products__element-navbar-link-orders>Meus Pedidos</p>
+      <p customer_products__element-navbar-user-full-name>Nome</p>
+      <p customer_products__element-navbar-link-logout>Sair</p>
+    </nav>
+  );
+}
+
+export default Header;
--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Products() {
   return (
-    <h1>Products</h1>
+    <main>
+      <Header />
+      <h1>Products</h1>
+    </main>
   );
 }
 
