795b6ee99d69f51f096d35e343b4a50c7147acb5
Marcos Campos <marxmyers@icloud.com>
2022-09-23 16:02:13 -0300

    feat: request products

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import { requestProducts } from '../services/request';
 
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await requestProducts('/products');
+      console.log(response);
+      setProducts(response);
+    }
+    getProducts();
+  }, []);
   return (
     <main>
       <Header />
       <h1>Products</h1>
+      { products?.map((product) => (
+        <li key={ product.id }>{product.name}</li>
+      ))}
     </main>
   );
 }
