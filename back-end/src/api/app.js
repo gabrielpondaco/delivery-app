3493b9addb48f75345b60b02bb1782155b79786c
Marcos Campos <marxmyers@icloud.com>
2022-09-26 18:42:58 -0300

    fix: lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,7 +4,7 @@ const { loginRouter, productRouter, salesRouter } = require('../routes');
 const app = express();
 
 app.use(express.json());
-app.use('/images', express.static('public'))
+app.use('/images', express.static('public'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -18,6 +18,5 @@ app.use('/', productRouter);
 app.use('/', salesRouter);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
-
 
 module.exports = app;
