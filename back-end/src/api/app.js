87eb7f2d1babf8ee564efc32e6c5f58b9d426e77
Marcos Campos <marxmyers@icloud.com>
2022-09-26 18:35:46 -0300

    feat: products route

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,6 +4,7 @@ const { loginRouter, productRouter, salesRouter } = require('../routes');
 const app = express();
 
 app.use(express.json());
+app.use('/images', express.static('public'))
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -17,5 +18,6 @@ app.use('/', productRouter);
 app.use('/', salesRouter);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
 
 module.exports = app;
