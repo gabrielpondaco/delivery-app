3d64e8164c2111d605a43bdd0ccb5bbc0d1e5c3c
rcolombodev <rcolombodev@gmail.com>
2022-09-27 10:41:57 -0300

    feat: cria componente com a tabela de detalhes

--- front-end/src/components/ProductTableLine.js
+++ front-end/src/components/ProductTableLine.js
@@ -1,45 +1,51 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import ProductTableItem from './ProductTableItem';
 
-function ProductTableLine({ index, description, quantity, unitPrice }) {
+function ProductTableLine() {
+  const productsDetails = [
+    {
+      id: 1,
+      description: 'xablau',
+      quantity: 2,
+      unitPrice: '16.00',
+    },
+    {
+      id: 2,
+      description: 'xablau2',
+      quantity: 3,
+      unitPrice: '10.00',
+    },
+  ];
+
+  const Items = productsDetails.map((product, index) => {
+    const { id, description, quantity, unitPrice } = product;
+    return (
+      <ProductTableItem
+        key={ id }
+        index={ index }
+        description={ description }
+        quantity={ quantity }
+        unitPrice={ unitPrice }
+      />
+    );
+  });
+
   return (
-    <main>
-      <span
-        data-testid={ `seller_order_details__element-order-table-item-number-${index}` }
-      >
-        {index}
-      </span>
-      <span
-        data-testid={ `seller_order_details__element-order-table-name-${index}` }
-      >
-        {description}
-      </span>
-      <span
-        data-testid={ `seller_order_details__element-order-table-quantity-${index}` }
-      >
-        {quantity}
-      </span>
-      <span
-        data-testid={ `seller_order_details__element-order-table-unit-price-${index}` }
-      >
-        R$
-        {unitPrice}
-      </span>
-      <span
-        data-testid={ `seller_order_details__element-order-table-sub-total-${index}` }
-      >
-        R$
-        {unitPrice * quantity}
-      </span>
-    </main>
+    <table className="detailsTable">
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Descrição</th>
+          <th>Quantidade</th>
+          <th>Valor Unitário</th>
+          <th>Sub-total</th>
+        </tr>
+      </thead>
+      <tbody>
+        { Items }
+      </tbody>
+    </table>
   );
 }
-
-ProductTableLine.propTypes = {
-  index: PropTypes.number.isRequired,
-  quantity: PropTypes.number.isRequired,
-  unitPrice: PropTypes.number.isRequired,
-  description: PropTypes.string.isRequired,
-};
 
 export default ProductTableLine;
