1505234c07893a1f0be4d2d4ca74ee86dc94c967
rcolombodev <rcolombodev@gmail.com>
2022-09-27 10:42:46 -0300

    feat: cria pagina de detalhes de seller

--- front-end/src/pages/SellerOrderDetails.js
+++ front-end/src/pages/SellerOrderDetails.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import Header from '../components/Header';
+import CardDetailsSeller from '../components/CardDetailsSeller';
+import ProductTableLine from '../components/ProductTableLine';
 
 function SellerOrdersDetails() {
   return (
-    <main>
-      <h1>Detalhes do Pedido</h1>
-    </main>
+    <>
+      <Header />
+      <main>
+        <div className="details-center-card">
+          <h1>Detalhe do Pedido</h1>
+          <CardDetailsSeller
+            id={ 1 }
+            status="Pendente"
+            saleDate={ new Date().toLocaleDateString() }
+          />
+          <ProductTableLine />
+          {/* <h1>
+            Total:
+            {' '}
+            R$
+            {' '}
+            <span data-testid="seller_order_details__element-order-total-price">
+              { total }
+            </span>
+          </h1> */}
+        </div>
+      </main>
+    </>
   );
 }
 
