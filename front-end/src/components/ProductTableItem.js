b3bf3885cf4768976bc1589623d9c11edb1bebf1
rcolombodev <rcolombodev@gmail.com>
2022-09-27 10:41:14 -0300

    feat: cria componente de itens na tabela

---
+++ front-end/src/components/ProductTableItem.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function ProductTableItem({ index, description, quantity, unitPrice }) {
+  const subTotal = (Number(unitPrice) * Number(quantity)).toFixed(2);
+  return (
+    <tr>
+      <td
+        className="item-details"
+        data-testid={ `seller_orders__element-order-table-item-number-${index}` }
+      >
+        {index}
+      </td>
+      <td
+        className="description-details"
+        data-testid={ `seller_orders__element-order-table-name-${index}` }
+      >
+        {description}
+      </td>
+      <td
+        className="quantity-details"
+        data-testid={ `seller_orders__element-order-table-quantity-${index}` }
+      >
+        {quantity}
+      </td>
+      <td
+        className="unity-details"
+        data-testid={ `seller_orders__element-order-table-unit-price-${index}` }
+      >
+        {unitPrice}
+      </td>
+      <td
+        className="total-details"
+        data-testid={ `seller_orders__element-order-table-total-price-${index}` }
+      >
+        {subTotal}
+      </td>
+    </tr>
+  );
+}
+
+ProductTableItem.propTypes = {
+  index: propTypes.number.isRequired,
+  description: propTypes.string.isRequired,
+  quantity: propTypes.number.isRequired,
+  unitPrice: propTypes.string.isRequired,
+};
+
+export default ProductTableItem;
