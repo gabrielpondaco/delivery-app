d5a35b2a41921bcbca8668cec03201ccaee90a05
rcolombodev <rcolombodev@gmail.com>
2022-09-28 19:24:21 -0300

    feat: implementa requisições para detalhes de cliente

--- front-end/src/pages/CustomerOrderDetails.js
+++ front-end/src/pages/CustomerOrderDetails.js
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import CardDetailsCustomer from '../components/CardDetailsCustomer';
 import ProductTableLine from '../components/ProductTableLine';
+import { requestGet } from '../services/request';
 
 function CustomerOrdersDetails() {
+  const [orderDetails, setOrderDetails] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchOrderDetails = async () => {
+      const response = await requestGet(`orders/${id}`);
+      setOrderDetails(response);
+    };
+    fetchOrderDetails();
+  }, [id]);
   return (
     <>
       <Header />
       <main>
         <div className="details-center-card">
           <h1>Detalhe do Pedido</h1>
-          <CardDetailsCustomer
-            id={ 1 }
-            status="Pendente"
-            saleDate={ new Date().toLocaleDateString() }
-          />
-          <ProductTableLine />
-          {/* <h1>
+          {orderDetails ? (
+            <>
+              <CardDetailsCustomer
+                id={ id }
+                status={ orderDetails.status }
+                saleDate={ orderDetails.saleDate }
+                sellerId={ orderDetails.sellerId }
+              />
+              <ProductTableLine products={ orderDetails.productsInfo } />
+            </>) : ''}
+
+          <h1>
             Total:
             {' '}
             R$
             {' '}
-            <span data-testid="seller_order_details__element-order-total-price">
-              { total }
+            <span data-testid="customer_order_details__element-order-total-price">
+              { orderDetails ? orderDetails.productsInfo
+                .reduce((acc, { price, quantity }) => (
+                  acc + (price * quantity)), 0)
+                .toFixed(2).toString().replace('.', ',') : '0,00' }
             </span>
-          </h1> */}
+          </h1>
         </div>
       </main>
     </>
